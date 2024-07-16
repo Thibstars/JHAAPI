ad31bbc28488dd59c8184e46738686ccf1a7509b
Thibstars <thibault.helsmoortel@gmail.com>
2024-07-16 17:01:52 +0200

    Add logging

--- src/main/java/com/github/thibstars/jhaapi/client/config/ConfigServiceImpl.java
+++ src/main/java/com/github/thibstars/jhaapi/client/config/ConfigServiceImpl.java
@@ -3,11 +3,15 @@ package com.github.thibstars.jhaapi.client.config;
 import com.github.thibstars.jhaapi.Configuration;
 import com.github.thibstars.jhaapi.client.BaseService;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Thibault Helsmoortel
  */
 public class ConfigServiceImpl extends BaseService<Config> implements ConfigService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigServiceImpl.class);
 
     public ConfigServiceImpl(Configuration configuration) {
         super(configuration, "config", Config.class);
@@ -15,6 +19,8 @@ public class ConfigServiceImpl extends BaseService<Config> implements ConfigServ
 
     @Override
     public Optional<Config> getConfig() {
+        LOGGER.info("Getting config");
+
         return getObject();
     }
 }
